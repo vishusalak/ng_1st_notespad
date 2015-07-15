dc5c40ecd1f371c69324238312bd11867ba0bc3e
Vishesh Shalak <vishusalak@users.noreply.github.com>
2015-07-15 19:28:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 ﻿/// <reference path="../Scripts/angular-1.1.4.js" />
 
-/*#######################################################################
-  
-  Dan Wahlin
-  http://twitter.com/DanWahlin
-  http://weblogs.asp.net/dwahlin
-  http://pluralsight.com/training/Authors/Details/dan-wahlin
-
-  Normally like to break AngularJS apps into the following folder structure
-  at a minimum:
-
-  /app
-      /controllers      
-      /directives
-      /services
-      /partials
-      /views
-
-  #######################################################################*/
 var app = angular.module('myApp', ['ngRoute','ui.bootstrap','ngDragDrop']);
 //This configures the routes and associates each route with a view and a controller
 app.config(function ($routeProvider) {
